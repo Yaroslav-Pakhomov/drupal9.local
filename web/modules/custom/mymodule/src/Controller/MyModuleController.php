279d0c9da0096331aad3a8cdd4fdcd2039e0c15b
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-06-15 13:23:22 +0500

    добавление параграфа, добавление и отображение twig в render массиве.

--- web/modules/custom/mymodule/src/Controller/MyModuleController.php
+++ web/modules/custom/mymodule/src/Controller/MyModuleController.php
@@ -46,9 +46,27 @@ class MyModuleController extends ControllerBase
 
     $output['#markup'] = 'My Module Text!';
 
+    // <p class="paragraph">При необходимости заполните одну из форм!</p>
+    $output = [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => 'При необходимости заполните одну из форм!',
+      '#attributes' => [
+        'class' => ['paragraph'],
+      ],
+    ];
+
     // Drupal\mymodule\Form\MyModuleForm
     $output[] = \Drupal::formBuilder()->getForm(MyModuleForm::class);
     $output[] = \Drupal::formBuilder()->getForm(MyModuleSettingsForm::class);
+
+    $output[] = [
+      'description' => [
+        '#theme' => 'my-module-templatename',
+        '#varname1' => 'value varname1',
+        '#varname2' => 'value varname2',
+      ]
+    ];
 
     return $output;
   }
